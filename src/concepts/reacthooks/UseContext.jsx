7358b5596318ff8eba2a1d4e7aa462d7fc547b5d
davidBilson <davidbilson99@gmail.com>
2024-02-18 17:31:40 +0100

    added useContext notes

---
+++ src/concepts/reacthooks/UseContext.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const UseContext = () => {
+  return (
+    <>
+        
+    </>
+  )
+}
+
+export default UseContext
+// NOTES //
+// useContext is a React Hook used for consuming context that has been created by the React.createContext API. 
+// It allows functional components to subscribe to context changes. 
+// Context provides a way to pass data through the component tree without having to pass props down manually at every level.
+// So, useState is used for managing component-local state, while useContext is used for consuming context provided by a parent component. They serve different purposes and are not direct replacements for each other. However, you might use them together in some cases where you need both local component state and context data within the same component
+// However, when you need to share state or functions between components that are not directly connected through parent-child relationships, you can use React's Context API along with useContext to achieve this.
+// With the Context API, you can create a context object that holds some data or functions, and then use useContext to access that context from any component in the application, regardless of its position in the component tree.
+// So, while useState is used for managing local component state, useContext is used for accessing shared data or functions throughout the application via the Context API. These two hooks serve complementary purposes and are often used together to manage both local and shared state in React applications.
+
+// Finally Context provides a way to pass data through the component tree without having to pass props down manually at every level

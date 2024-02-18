aaa5e13c0fafa21149f425f26e2b0c2be276f386
davidBilson <davidbilson99@gmail.com>
2024-02-18 13:25:53 +0100

    Done with useEffect

--- src/concepts/reacthooks/UseEffectFetchData.jsx
+++ src/concepts/reacthooks/UseEffectFetchData.jsx
@@ -4,12 +4,16 @@ import axios from 'axios';
 const UseEffectFetchData = () => {
   const [data, setData] = useState({})
   const [id, setId] = useState(1);
+  const [idOnclick, setIdOnclick] = useState(1);
 
+  const fetchData = () => {
+    setIdOnclick(id)
+  }
    useEffect(() => {
-    async () => {
+    (async () => {
       try {
-        const res = await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
-        console.log(res)
+        const res = await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`);
+        console.log(res);
         if (res.status === 200) {
           setData(res.data);
         } else {
@@ -18,13 +22,13 @@ const UseEffectFetchData = () => {
       } catch (error) {
         console.log(error)
       }
-    }
-  }, [id])
+    })();
+  }, [idOnclick]);
 
-  // getData()
   return (
     <>
       <input type="text" placeholder='Enter Item Id' value={id} onChange={(e) => setId(e.target.value)} />
+      <button onClick={fetchData}>Fetch Data</button>
       <div>{data.id} {data.title} --</div>
       {/* <ul>
         {data.map(item => (
